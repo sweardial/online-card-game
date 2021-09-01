6e5ef8984c8a2241c77950109f682a56239e23d5
lleonore <chesnokovdmytro@gmai.com>
2021-09-01 20:15:57 +0300

    fix

--- server/index.js
+++ server/index.js
@@ -103,9 +103,9 @@ const socketHandler = socket => {
 io.on('connection', socketHandler);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.resolve('../client/build')));
+  app.use(express.static(path.join(__dirname, '../', 'client', 'build')));
   app.get('*', (req, res) => {
-    res.send('WORKING');
+    res.sendFile(path.join(__dirname, '../', 'client', 'build', 'index.html'));
   });
 }
 
