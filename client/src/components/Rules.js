14ebc3132cded55134f6afa1e310d83596b72c94
lleonore <chesnokovdmytro@gmai.com>
2021-09-02 03:27:14 +0300

    text fix

--- client/src/components/Rules.js
+++ client/src/components/Rules.js
@@ -17,18 +17,18 @@ const Rules = () => {
         your hand back up to 6. </p>
 
         <p>
-        When it's your turn to attack,
+        <b>When it's your turn to attack,</b>
         you start with any card, and then, if you have card with an equal value that you see any card on the battle table,
-        you can toss your card in. As a defending player your main goal is to beat attacking cards.</p>
+        you can toss your card in. <b>As a defending player</b> your main goal is to beat attacking cards.</p>
 
         <p>
-        If you are using a card with
-        a trump suit - you can beat any other cards regardless of its value (if it's not againt a trump suited card. 
+        <b>If you are using a card with
+        a trump suit</b> - you can beat any other cards regardless of its value (if it's not againt a trump suited card. 
         In this case value pattern is the same)
-        If you don't have a card that beats the attacking card, then you grab all of the cards on the battle table, 
+        <b>If you don't have a card that beats the attacking card</b>, then you grab all of the cards on the battle table, 
         including some cards from attacking player
         that he can toss in after you decided to grab.</p>
-        <b>Value pattern</b>
+        <b>Value pattern:</b>
         <br></br>
         2 is smaller than 3<br></br>
         3 is smaller than 4<br></br>
@@ -40,10 +40,10 @@ const Rules = () => {
         Jack is smaller than Queen<br></br>
         Queen is smaller than King<br></br>
         King is smaller that Ace<br></br>
-        <br></br>
         <p>
-        Game is over when the deck is empty one of the users don't have any cards left. 
-        If both of players don't have cards - it's a Tie.
+        <b>Game is over</b> when the deck is empty one of the users don't have any cards left. 
+        Empty head player is a <b>winner</b>.
+        If both of players don't have cards - it's a <b>Tie</b>.
         </p>
     </div>
   )
